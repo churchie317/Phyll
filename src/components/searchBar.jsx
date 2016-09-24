0426a2c30b1c47d47149181789b135544e9f4edb
Eric Churchill <eric.churchill2@gmail.com>
2016-09-23 19:11:07 -0700

    [feature] Update components to work in home view

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,6 +1,5 @@
 import Autosuggest from 'react-autosuggest';//Reference:  https://github.com/moroshko/react-autosuggest
 import './theme.css';
-import {records} from '../records.js';
 
 // Revisit to improve search performance and implement fetch plant data function for plantFacts component use
 const plantsName = records.map((plant) => {
